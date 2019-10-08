21b5afbc76ca852641850dbda948783d859715fc
joshseyda <josh.seyda@gmail.com>
2019-10-08 14:01:06 -0400

    header exists test is passing

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -4,6 +4,6 @@ describe('Display list of employees', () => {
   it('when user visits the page', () => {
     cy.visit('http://localhost:3000')
     cy.get('section[name="header"]')
-      .should('contain', 'Employee list')
+      .should('contain', 'Employee List')
   })
 })
