f1041db88f6980ab04a34d6a78143948d578219e
joshseyda <josh.seyda@gmail.com>
2019-10-08 13:59:32 -0400

    url error

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -2,7 +2,7 @@
 
 describe('Display list of employees', () => {
   it('when user visits the page', () => {
-    cy.visit('http://localhost"3000')
+    cy.visit('http://localhost:3000')
     cy.get('section[name="header"]')
       .should('contain', 'Employee list')
   })
